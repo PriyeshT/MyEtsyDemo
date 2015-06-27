58f9a0e661bf35906dcbf088d6bb8d6dd10bbb3e
PriyeshT <priyeshtungare31@gmail.com>
2015-06-27 15:36:27 +0800

    Changed storage details
    Changes to storage details for development and production environments

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,14 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-					  :storage => :dropbox,
-    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	else
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+						  :storage => :dropbox,
+    					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    					  :path => ":style/:id_:filename",
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	end
 
 end
