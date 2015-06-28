d25d0b45944c5f96b9369ce90a68877c60836809
PriyeshT <priyeshtungare31@gmail.com>
2015-06-28 12:03:24 +0800

    Added paperclip required image validation check

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,13 +2,13 @@ class Listing < ActiveRecord::Base
 
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
-		#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
 						  :storage => :dropbox,
     					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     					  :path => ":style/:id_:filename"
-		#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	end
 
 end
