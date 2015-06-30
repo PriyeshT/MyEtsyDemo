eed042644ed187cc095a64c8c5eb6ca7ea81b8ed
PriyeshT <priyeshtungare31@gmail.com>
2015-06-30 21:34:31 +0800

    Changes to production deployment

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,19 +1,19 @@
 class Listing < ActiveRecord::Base
 
-	#if Rails.env.development?
+	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
 		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 		belongs_to :user
-	#else
-	#	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
-	#					  :storage => :dropbox,
-    #					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    #					  :path => ":style/:id_:filename"
-	#	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-#
-	#	belongs_to :user
-	#end
+	else
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
+						  :storage => :dropbox,
+    					  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    					  :path => ":style/:id_:filename"
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+		belongs_to :user
+	end
 
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: { greater_than: 0}
